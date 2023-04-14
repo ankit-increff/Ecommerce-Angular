b0f7107547dffafa1c18a7817aa030fa4356ef15
ankit-increff <ankit.kumar@increff.com>
2023-04-14 18:51:02 +0530

    added filters

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -13,15 +13,27 @@ import { CartService } from '../../services/cart.service';
 })
 export class ProductsComponent {
   closeResult = '';
-
+  
   constructor(private offcanvasService: NgbOffcanvas, private productService: ProductService, private usersService: UsersService, private cartService: CartService) { }
-
+  
   products: product[] = [];
   quantities: quantityArray = {}
+  sortBy = 'Relevance';
+  filters = {
+    minPrice: undefined,
+    maxPrice: undefined,
+    brands:[] as string[],
+    rating: 0
+  }
+  allBrands:string[] = [];
+
   ngOnInit() {
     console.log('prodInitStarted');
     this.productService.products.subscribe(data => {
+      console.log('subscribed');
       this.products = data.products;
+      this.allBrands = this.products.map(product => product.brand)
+      this.allBrands = [...new Set(this.allBrands)];
       console.log("prod - syncedProducts");
     })
 
@@ -63,6 +75,59 @@ export class ProductsComponent {
     this.cartService.updateCart(id, parseInt($event.target.value))
   }
 
-  sort = 'Relevance';
+  changeSorting(sort: string) {
+    this.sortBy = sort;
+    this.applyFilters();
+  }
+
+  applySorting(sort:string) {
+    this.products = [...this.productService.productsData];
+    switch (sort) {
+      case 'Relevance':
+        this.products = this.productService.productsData
+        break;
+      case 'Price low to high':
+        this.products = this.products.sort((a:product, b:product) => a.price - b.price);
+        break;
+        case 'Price high to low':
+          this.products = this.products.sort((a:product, b:product) => b.price - a.price);
+          break;
+        case 'Ratings':
+          this.products = this.products.sort((a:product, b:product) => b.rating - a.rating);
+          break;
+    }
+  }
+
+  applyFilters() {
+    this.applySorting(this.sortBy);
+    if(this.filters.minPrice) this.products = this.products.filter(product => product.price >= (this.filters.minPrice || 0));
+    if(this.filters.maxPrice) this.products = this.products.filter(product => product.price <= (this.filters.maxPrice|| 999999));
+    if(this.filters.rating) this.products = this.products.filter(product => product.rating >= (this.filters.rating || 0))
+    console.log(this.products, this.filters.brands);
+    if(this.filters.brands.length) this.products = this.products.filter(product =>this.filters.brands.includes(product.brand));
+  }
+
+  changeMinPrice(event:any) {
+    this.filters.minPrice = event.target.value;
+    this.applyFilters();
+  }
+  
+  changeMaxPrice(event:any) {
+    this.filters.maxPrice = event.target.value
+    this.applyFilters();
+  }
+
+  ratingChangeHandler(data:any) {
+    this.filters.rating = parseInt(data.rating)
+    this.applyFilters();
+  } 
+
+  brandChangeHandler(data:any) {
+    let brandFilters:string[] = [];
+    for(let i in data) {
+      if(data[i] ==true) brandFilters.push(i);
+    }
+    this.filters.brands = brandFilters;
+    this.applyFilters();
+  }
 }
-// todo update cart handler
